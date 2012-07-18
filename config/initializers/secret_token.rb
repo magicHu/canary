6cf9c3c5f13b0fdfcfbafb57b3002b21b4b51b5b
Magic <huronghai2008@gmail.com>
2012-07-18 18:32:17 +0800

    init cannary

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Canary::Application.config.secret_token = '1b08a901662f0f0b24f0e78a9ceeb82f29753294974a8d4e9ad325946482cebcdfa529d3ab3716e40d669adc2f18277a38c4e9e4024a4a98e2ec246b82479677'
