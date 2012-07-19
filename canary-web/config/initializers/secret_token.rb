88ebc27d4c5a381efa15906143541f7ef406eff0
Magic <huronghai2008@gmail.com>
2012-07-19 22:40:39 +0800

    add web project

---
+++ canary-web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CanaryWeb::Application.config.secret_token = '93b42939b695bf2edec77b1a3760acee7d79dc9f4119492fa991c0a9460ff9a062db564c9e3d158fc4c32e35907101ea3e6891e6ed4f7e8c1058c0057535640d'
