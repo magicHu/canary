d988e11c73a166b85ae9ea4bbce5aa41749bf4b5
liyijie <liyijie825@gmail.com>
2012-12-05 00:09:22 +0800

    modify time zone for 'beijing', avoid time is delay 8 hours

--- canary-web/config/application.rb
+++ canary-web/config/application.rb
@@ -58,5 +58,9 @@ module CanaryWeb
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Time zone
+    config.time_zone = 'Beijing'  
+    config.active_record.default_timezone = :Beijing  
   end
 end
