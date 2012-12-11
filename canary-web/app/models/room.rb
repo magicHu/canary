063cb3f0001ff1e10353f2a25c4be8e45c6a5352
MagicHu <huronghai2008@gmail.com>
2012-12-11 22:55:45 +0800

    use act-as-commentable gem

--- canary-web/app/models/room.rb
+++ canary-web/app/models/room.rb
@@ -1,13 +1,18 @@
 class Room < ActiveRecord::Base
   attr_accessible :address, :desc, :name, :price, :total, :attachments_attributes
 
-  has_many :orders  
-  has_many :attachments, :as => :attachmentable, :dependent => :destroy
-  accepts_nested_attributes_for :attachments, :allow_destroy => true
-
   validates :address, :desc, :name, :price, :total, :presence => true
   validates_numericality_of :price, :total, :message => "is not a number"
 
+  # attachment
+  has_many :attachments, :as => :attachmentable, :dependent => :destroy
+  accepts_nested_attributes_for :attachments, :allow_destroy => true
   default_scope includes(:attachments)
 
+  # orders
+  has_many :orders  
+
+  # comment
+  acts_as_commentable
+  
 end
--- canary-web/db/schema.rb
+++ canary-web/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121211093839) do
+ActiveRecord::Schema.define(:version => 20121211145222) do
 
   create_table "attachments", :force => true do |t|
     t.string   "file_name"
@@ -36,6 +36,20 @@ ActiveRecord::Schema.define(:version => 20121211093839) do
     t.integer  "position"
     t.string   "state"
   end
+
+  create_table "comments", :force => true do |t|
+    t.string   "title",            :limit => 50, :default => ""
+    t.text     "comment"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.integer  "user_id"
+    t.datetime "created_at",                                     :null => false
+    t.datetime "updated_at",                                     :null => false
+  end
+
+  add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
+  add_index "comments", ["commentable_type"], :name => "index_comments_on_commentable_type"
+  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
 
   create_table "orders", :force => true do |t|
     t.integer  "user_id"
